7f7befe555626547723a7da8be38c5f660daf137
Yadhu <yadhuy28@gmail.com>
2024-06-21 01:20:26 +0530

    Basic operations donegit add .!

--- src/App.jsx
+++ src/App.jsx
@@ -12,26 +12,31 @@ function App() {
     const response = await fetch(`https://nominatim.openstreetmap.org/search?format=json&q=${encodeURIComponent(location)}`);
     const dataa = await response.json();
 
-    console.log('geoLocation = ',dataa)
+    // console.log('geoLocation = ',dataa)
     const {lat,lon} = dataa[0]
+    console.log('lat=',lat,' lon=',lon)
     setCoordinates({lat,lon})
+
+    console.log('coordinates = ',coordinates)
+    getData(lat,lon)
   }
 
   const API_key = import.meta.env.VITE_API_KEY
-  const api_link = `https://api.openweathermap.org/data/2.5/weather?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${API_key}`
   // const api_link = `https://api.openweathermap.org/data/2.5/weather?lat=44.34&lon=10.99&appid=${API_key}`
-  function getData(){
+  function getData(lat,lon){
+  const api_link = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_key}`
     axios.get(api_link)
     .then((res)=>{
-      console.log(res.data)
+      console.log(res)
+      setData(res.data)
     })
     .catch((err)=>{
       console.error(err);
     })
   }
-  useEffect(()=>{
-    getData();
-  },[])
+// useEffect(()=>{
+//   console.log('data = ',data)
+// },[data])
 
   // console.log(navigator.geolocation)
 
@@ -52,26 +57,28 @@ function App() {
       <div className="container" >
         <div className="top">
           <div className="location">
-            <p>Kanodi</p>
+            <p>{data.name || "Hi there"}</p>
           </div>
         <div className="temp">
-          <h1>60'F</h1>
+        {data.main?<h1>{data.main.temp}'F</h1>:<h1>How is the weather today</h1>}
+          
         </div>
         <div className="description">
-          <p>Clouds</p>
+         {data.weather? <p>{data.weather[0].main}</p> : null}
         </div>
         </div>
         <div className="bottom">
           <div className="feels">
-            <p>60'F</p>
+            {data.main?<p>{data.main.feels_like}'F</p>:null}
             <p>feels like</p>
           </div>
           <div className="humidity">
-            <p>20%</p>
+          {data.main?<p>{data.main.humidity}%</p>:null}
             <p>Humidity</p>
           </div>
           <div className="wind">
-            <p>120MPH</p>
+            {data.wind?<p>{data.wind.speed}MPH</p>:null}
+
             <p>wind speed</p>
           </div>
         </div>
